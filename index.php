1275743c5bac03fd553c0456b9f78e86734f41bf
Tobias Hvass Mølbak <tobiashmhtx@gmail.com>
2016-10-25 21:00:32 +0200

    Updated name header

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <title>Molbak</title>
+  <title>Ettrup</title>
   
   <link rel="shortcut icon" href="/images/favicon.ico"/>
   <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
